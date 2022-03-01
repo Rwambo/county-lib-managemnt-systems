1b3caf3fcb390337353b6fa5e71a05cb5e7440b3
Rwambo <88381936+Rwambo@users.noreply.github.com>
2022-03-01 13:03:32 +0300

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,23 @@
+/* 
+  Project: County Library Management System.
+  Author: Esther Waiganjo.
+  Date: Oct, 2021.
+  Compiler: GNU GCC
+  Language: C99
+  license: MIT
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	printf("\tCounty Library\n");
+	printf("Welcome Miss Esther.\n");
+	printf("What would you like to do?\n");
+	printf("1. Add new user\n");
+	printf("2. Add book\n");
+	printf("selected action: ");
+	
+	return 0;
+}
